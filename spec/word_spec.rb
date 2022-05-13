79ad4ff8056f6fc2fa9afcdc001a5db013183e1f
Mike Wright <m.wright92@icloud.com>
2022-05-13 08:17:55 -0700

    initialize repo for word definer project, WIP .all method for Word class

---
+++ spec/word_spec.rb
@@ -0,0 +1,11 @@
+require 'pry'
+require 'rspec'
+require 'word'
+
+describe('#Word') do
+  describe('.all') do
+    it('returns an empty array when there are no words') do
+      expect(Word.all).to(eq([]))
+    end
+  end
+end
