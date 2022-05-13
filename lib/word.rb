9d3ae51a5840035de409293ef34846e954f0d532
Mike Wright <m.wright92@icloud.com>
2022-05-13 08:30:23 -0700

    second test fail, WIP build == method for comparing words

--- lib/word.rb
+++ lib/word.rb
@@ -1,12 +1,18 @@
 class Word
+  attr_reader :name, :id
   @@words = {}
+  @@total_rows = 0
 
   def initialize(name, id)
     @name = name
-    @id = id
+    @id = id || @@total_rows += 1
   end
 
   def self.all
     @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new(self.name, self.id)
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -8,4 +8,12 @@ describe('#Word') do
       expect(Word.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it('saves a word to the the word class') do
+      word1 = Word.new('hello', nil)
+      word1.save
+      expect(Word.all).to(eq(word1))
+    end
+  end
 end
