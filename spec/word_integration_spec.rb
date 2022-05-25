f2ffd4db02309165e053c456b0941ed7a99482a1
Mike Wright <m.wright92@icloud.com>
2022-05-24 20:33:12 -0700

    pass first integration spec

---
+++ spec/word_integration_spec.rb
@@ -0,0 +1,30 @@
+require('capybara/rspec')
+require('./app.rb')
+require('pry')
+
+Capybara.app = Sinatra::Application
+set(:show_exeptions, false)
+
+describe('the word path', {:type => :feature}) do
+  before(:each) do
+    Word.clear
+    Definition.clear
+    @word = Word.new("hello", nil)
+    @word.save
+  end
+
+  it('creates a word') do
+    visit('/')
+    click_on('Add a new word')
+    fill_in('word_input', :with => "goodbye")
+    click_on('Go!')
+    expect(page).to have_content("goodbye")
+  end
+
+  it('updates a word') do
+    visit('/words')
+    click_on("goodbye")
+    click_on("Edit word")
+    fill_in("")
+  end
+end
